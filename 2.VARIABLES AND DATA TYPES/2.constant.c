c9ca0c28c041180320097b03060b9f2811011613
ayush-tomer <ayushtomer012@gmail.com>
2024-12-22 13:58:27 +0530

    Update

---
+++ 2.VARIABLES AND DATA TYPES/2.constant.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define AGE 20
+int main()
+{
+    const float PI = 3.14159;
+    printf("Age: %d\n", AGE);
+    printf("PI: %f", PI);
+}
---
+++ 2.VARIABLES AND DATA TYPES/3.escape.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Ayush\nTomer\n");
+    printf("Ayush\tTomer\n");
+    printf("Ayush\bTomer\n");
+    printf("Ayush\'Tomer\'\n");
+    printf("Ayush\"Tomer\"\n");
+    printf("Ayush\\Tomer\n");
+    return 0;
+}
